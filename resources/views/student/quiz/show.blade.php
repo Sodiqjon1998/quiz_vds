0bec4fe2f58c61777442a27bf4c6781d823ce36e
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-07-16 16:09:02 +0500

    fixes

--- resources/views/student/quiz/show.blade.php
+++ resources/views/student/quiz/show.blade.php
@@ -88,6 +88,8 @@
         #options-form {
             display: flex;
             flex-direction: column;
+            align-items: flex-start;
+            justify-content: flex-start;
         }
 
         .option-item {
