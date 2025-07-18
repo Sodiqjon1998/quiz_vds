7e840c9cdf5d0dd8b3a8028acb18a5e7db53f6af
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-07-18 17:10:09 +0500

    teacher section change

--- resources/views/teacher/site/index.blade.php
+++ resources/views/teacher/site/index.blade.php
@@ -46,6 +46,29 @@
                 </div>
             </div>
 
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-info shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
+                                    Diskda Band Joy</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                    @if (isset($diskSpace['used']))
+                                        {{ $diskSpace['used'] }} qoldi ({{ $diskSpace['usage_percent'] }} ishlatilgan)
+                                    @else
+                                        Ma'lumot mavjud emas
+                                    @endif
+                                </div>
+                            </div>
+                            <div class="col-auto">
+                                <i class="fas fa-hdd fa-2x text-gray-300"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             {{-- Boshqa statistikalar va kontent --}}
 
         </div>
