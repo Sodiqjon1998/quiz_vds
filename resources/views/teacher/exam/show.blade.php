8c54d4f652e313f93931f4d3a40f6e54c2fc8982
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-07-28 05:23:13 +0500

    url fix

--- resources/views/teacher/exam/show.blade.php
+++ resources/views/teacher/exam/show.blade.php
@@ -72,8 +72,8 @@
                                             $all = Exam::allQuestions($item->id);
                                             echo $all > 0 ? round(($correct / $all) * 100, 2) : 0;
                                         @endphp
+                                        %
                                     </strong>
-                                    %
                                 </td>
                                 <td>
                                     {{ $item->created_at }}
