09bce04051e67ec3062e9ed8eff6e4cd2068d4f4
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-11-25 01:55:18 +0500

    nnnnnnnnn

--- config/cors.php
+++ config/cors.php
@@ -2,18 +2,11 @@
 
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
     'allowed_methods' => ['*'],
-
-    // MANA SHU YERGA E'TIBOR BERING:
-    'allowed_origins' => ['*'], // '*' qilsangiz hamma joydan ruxsat beradi (Test uchun eng osoni)
-
-    // Yoki xavfsizroq varianti (o'zingizni domenni yozasiz):
-    // 'allowed_origins' => ['http://localhost:5173', 'https://sizning-react-saytingiz.railway.app'],
-
+    'allowed_origins' => ['*'], // <-- Eng muhimi shu! Hamma joydan ruxsat berish.
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Agar cookie/session ishlatsangiz true bo'lishi kerak
+    'supports_credentials' => true,
 ];
