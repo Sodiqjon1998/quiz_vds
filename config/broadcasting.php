21898754dc548f0402f4103804340ad03337ebf7
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-12-09 22:48:56 +0500

    fdjbsnsfvsdvsdvsdvds

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,25 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
-                'encrypted' => true,
+                'cluster' => env('PUSHER_APP_CLUSTER'),
                 'useTLS' => true,
-
-                // ✅ RAILWAY UCHUN
-                'host' => env('PUSHER_HOST') ?: 'api-' . env('PUSHER_APP_CLUSTER', 'ap1') . '.pusher.com',
-                'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'https'),
-
-                'curl_options' => [
-                    CURLOPT_SSL_VERIFYHOST => 2,
-                    CURLOPT_SSL_VERIFYPEER => true,
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_TIMEOUT => 60,
-                ],
-            ],
-            'client_options' => [
-                // ✅ Guzzle client options
-                'timeout' => 60,
+                'encrypted' => true,
             ],
         ],
 
--- config/cors.php
+++ config/cors.php
@@ -1,15 +1,12 @@
 <?php
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'backend/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => [
-        'https://quizvds.up.railway.app',
-        'https://andijonyuksalish.netlify.app',
-        'http://localhost:5173',
-    ],
+    'allowed_origins' => ['*'], // <-- Eng muhimi shu! Hamma joydan ruxsat berish.
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true, // Bu muhim!
+    'supports_credentials' => true,
+
 ];
