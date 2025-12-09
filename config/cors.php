59b7f1c40a1fae890915ea4984441809e70f7130
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-12-09 14:08:07 +0500

    yyyy

--- config/cors.php
+++ config/cors.php
@@ -1,20 +1,14 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['*'], // <-- Eng muhimi shu! Hamma joydan ruxsat berish.
     'allowed_origins_patterns' => [],
-    'allowed_headers' => [
-        'Content-Type',
-        'X-Requested-With',
-        'Authorization',
-        'X-CSRF-TOKEN',
-        'X-Socket-Id',  // ✅ Pusher uchun kerak
-        'Accept',
-    ],
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => true,
-
+    
 ];
+
