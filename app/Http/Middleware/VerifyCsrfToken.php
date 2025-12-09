c1abe24a740d55e19045d9df48ac6a52fcbfb549
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-12-09 14:41:35 +0500

    eeeeettttttttgndgfndfgdfdbdfbdfbdbdfbdbdfbadf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,9 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'backend/login',      // ✅ Login uchun
-        'backend/*',          // ✅ Barcha backend route'lari uchun
-        'teacher/login',
-        'api/*',
+        // 'backend/login',      // ✅ Login uchun
+        // 'backend/*',          // ✅ Barcha backend route'lari uchun
+        // 'teacher/login',
+        // 'api/*',
     ];
 }
