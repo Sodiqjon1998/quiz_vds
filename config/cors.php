cfe1c10bd1db19dcb95d16b4b7c9b11cff6cbe7e
Sodiqjon1998 <sirojiddinovsodiqjon1998@gmail.com>
2025-12-09 14:24:30 +0500

    eeeee

--- config/cors.php
+++ config/cors.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'backend/*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://quizvds.up.railway.app',
